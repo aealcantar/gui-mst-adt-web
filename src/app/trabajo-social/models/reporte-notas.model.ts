b9de75b8264a25daf7770343aae65699f6d2939b
lfgregorio <105452956+lfgregorio@users.noreply.github.com>
2022-06-17 15:35:37 -0500

    se agregan models y services

---
+++ src/app/trabajo-social/models/reporte-notas.model.ts
@@ -0,0 +1,31 @@
+export class ReporteNota {
+    nss?: string;
+	aMedico?: string;
+	nombrePaciente?: string;
+	fechaN?: string;
+	unidad?: string;
+	ooad?: string;
+	curp?: string;
+	unidad2?: string;
+	consultorio?: string;
+	servicio?: string;
+	turno?: string;
+	nombreTS?: string;
+	matriculaTS?: string;
+	nombreTS2?: string;
+	matriculaTS2?: string;
+	tipoNota?: string;
+	redApoyo?: string;
+	actividadTecnica?: string;
+	diagnostico?: string;
+	redaccion?: string;
+	tipoNota2?: string;
+	redApoyo2?: string;
+	actividadTecnica2?: string;
+	diagnostico2?: string;
+	redaccion2?: string;
+	fecha1?: string;
+	fecha2?: string;
+	diagnosticoSocial?: string;
+	diagnosticoSocial2?: string;
+}
---
+++ src/app/trabajo-social/services/notas.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Nota } from '../models/notas.model';
+
+const urlServNotas = `${environment.urlServNotas}/msmts-notas/api`
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class NotasService {
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  getNotasByFechas(fechaInicial: string, fechaFinal: string) {
+    return this.http.get<any>(`${urlServNotas}/findNotasByFechas/${fechaInicial}/${fechaFinal}`, { responseType: 'json'});
+  }
+
+  getNotasById(id: number) {
+    return this.http.get<any>(`${urlServNotas}/getNotaTSById/${id}`, { responseType: 'json'});
+  }
+
+  addNota(nota: Nota) {
+    return this.http.post<Nota>(`${urlServNotas}/guardaNuevaNota`, nota);
+  }
+
+  updateNota(nota: Nota) {
+    return this.http.post<Nota>(`${urlServNotas}/actualizaNotaTs`, nota);
+  }
+
+  getTiposNota() {
+    return this.http.get<any>(`${urlServNotas}/getTiposDeNota`, { responseType: 'json'});
+  }
+
+  getRedesApoyo() {
+    return this.http.get<any>(`${urlServNotas}/getRedesSociales`, { responseType: 'json'});
+  }
+
+  getActividadesTecnicas() {
+    return this.http.get<any>(`${urlServNotas}/getActividadesTecnicas`, { responseType: 'json'});
+  }
+
+  // getDiagnosticosMedicos(texto: string) {
+  //   return this.http.get<any>(`${environment.urlServNotas}/registroNotasTs/getDiagnosticosMedicos/${texto}`, { responseType: 'json'});
+  // }
+
+  downloadPdf(data: any): Observable<Blob> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json', responseType: 'blob' });
+    return this.http.post<Blob>(`${urlServNotas}/reporteNotaTs`, JSON.stringify(data),
+    { headers: headers, responseType: 'blob' as 'json'});
+  }
+
+}
