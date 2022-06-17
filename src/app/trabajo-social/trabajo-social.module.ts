ff50cc71dc91eedec7adcb844fc21129859fd2c3
lfgregorio <105452956+lfgregorio@users.noreply.github.com>
2022-06-17 16:58:56 -0500

    se agrega busqueda nss

--- src/app/trabajo-social/trabajo-social.module.ts
+++ src/app/trabajo-social/trabajo-social.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrabajoSocialRoutingModule } from './trabajo-social-routing.module';
- import { NuevoControlArticulosComponent } from './control-articulos/nuevo-control-articulos/nuevo-control-articulos.component'; 
+ import { NuevoControlArticulosComponent } from './control-articulos/nuevo-control-articulos/nuevo-control-articulos.component';
 import { DetalleControlArticulosComponent } from './control-articulos/detalle-control-articulos/detalle-control-articulos.component';
 import { SharedModulesModule } from '../shared-modules/shared-modules.module';
+import { BusquedaNssComponent } from './pacientes/busqueda-nss/busqueda-nss.component';
+import { NgxPaginationModule } from "ngx-pagination";
 
 @NgModule({
   declarations: [
     NuevoControlArticulosComponent,
     DetalleControlArticulosComponent,
+    BusquedaNssComponent,
   ],
   imports: [
     SharedModulesModule,
     TrabajoSocialRoutingModule,
-    CommonModule
+    CommonModule,
+    NgxPaginationModule,
   ]
 })
 export class TrabajoSocialModule { }
