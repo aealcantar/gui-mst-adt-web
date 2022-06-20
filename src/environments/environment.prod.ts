ddd634b0d24fa2771029f3542cb84518ba4223f8
Ameyalli Victoria Sarmiento <avictoria@grupopm.mx>
2022-06-20 14:10:44 -0500

    enviroments.prod correctos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,38 +1,39 @@
 export const environment = {
-  production: true,
+    production: true,
 
 
 
-  //Agenda
-  urlLogin: 'msadts-gui/login',
-  urlMSADCargasCatalogos: 'msedsc-carga-catalogos',
-  urlMSEDSCatalogos: 'msedsc-catalogos/api',
-  urlMSADTCITAS: 'msadt-citas/api',
-  urlMSEDSCatalogosHorarios: 'msedsc-catalogos/api',
-  urlMSEDSCHorarios: 'msedsc-catalogos',
-  urlMSEDSCAgenda: 'msedsc-catalogos',
-  urlMSADTUSUARIOS: 'msedsc-usuarios/api',
+    //Agenda
+    urlLogin: 'msadts-gui/login',
+    urlMSADCargasCatalogos: 'msedsc-carga-catalogos',
+    urlMSEDSCatalogos: 'msedsc-catalogos/api',
+    urlMSADTCITAS: 'msadt-citas/api',
+    urlMSEDSCatalogosHorarios: 'msedsc-catalogos/api',
+    urlMSEDSCHorarios: 'msedsc-catalogos',
+    urlMSEDSCAgenda: 'msedsc-catalogos',
+    urlMSADTUSUARIOS: 'msedsc-usuarios/api',
 
 
-  //Trabajo Social
+    //Trabajo Social
+
+   
+    
+    msmtsCronicas: 'https://mts-qa.cloudapps.imss.gob.mx/msmts-cronica-grupal/api',
+    msmtsNotas: 'https://mts-qa.cloudapps.imss.gob.mx/msmts-notas/api',
+    msmtsEstudioMedicos: 'https://mts-qa.cloudapps.imss.gob.mx/msmts-estudios-medicos/api',
+    msmtsPacientes: 'https://mts-qa.cloudapps.imss.gob.mx/msmts-pacientes/api/busquedanss/',
+    msmtsCronicasReporte: 'https://mts-qa.cloudapps.imss.gob.mx/msmts-cronica-grupal/api',
+    msmtsCatalogos: 'msedsc-catalogos',
 
 
-  msmtsPacientes: 'msmts-pacientes/api/busquedanss/',
-  msmtsCronicasReporte: 'msmts-cronica-grupal/api',
-  msmtsCatalogos: 'msedsc-catalogos',
-  msmtsCronicas: 'msmts-cronica-grupal/api',
-  msmtsNotas: 'msmts-notas/api',
-  msmtsEstudioMedicos: 'msmts-estudios-medicos/api',
 
+    msmtsOauth: 'msadt-auth',
 
-
-  msmtsOauth: 'msadt-auth',
-
-  urlServOauth: 'msadt-auth',
-  urlSiteGoogleRecaptcha: 'https://www.google.com/recaptcha/api/siteverify',
-  recaptcha: {
+    urlServOauth: 'msadt-auth',
+    urlSiteGoogleRecaptcha: 'https://www.google.com/recaptcha/api/siteverify',
+    recaptcha: {
+        siteKey: '6LedF14gAAAAANjIISWT6m3vsMtfnrCOf5yBrajd',
+    },
     siteKey: '6LedF14gAAAAANjIISWT6m3vsMtfnrCOf5yBrajd',
-  },
-  siteKey: '6LedF14gAAAAANjIISWT6m3vsMtfnrCOf5yBrajd',
-  secretKey: '6LedF14gAAAAAKB180as1K6fb5xq8pa78R14FjgB'
+    secretKey: '6LedF14gAAAAAKB180as1K6fb5xq8pa78R14FjgB'
 };
