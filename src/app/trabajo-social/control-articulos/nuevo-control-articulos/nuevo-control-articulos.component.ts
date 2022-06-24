1d58628bfc31df044846e6f4e47749a7e113feea
LeandroPeopleMedia <107154425+LeandroPeopleMedia@users.noreply.github.com>
2022-06-23 21:58:41 -0500

    a

--- src/app/trabajo-social/control-articulos/nuevo-control-articulos/nuevo-control-articulos.component.ts
+++ src/app/trabajo-social/control-articulos/nuevo-control-articulos/nuevo-control-articulos.component.ts
@@ -50,12 +50,8 @@ export class NuevoControlArticulosComponent implements OnInit, AfterViewInit {
     personalQueElaboro: new FormControl(''),
     idCa: new FormControl(''),
     noFolioControl: new FormControl(''),
-<<<<<<< HEAD
     fecha: new FormControl('', [Validators.required,
     Validators.pattern(/(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{4})/)]),
-=======
-    fecha: new FormControl('', Validators.required),
->>>>>>> 8e8221c571a8c2dd708d166550f1b6a0b560c10f
     noCama: new FormControl('', Validators.required),
     servicio: new FormControl('', Validators.required),
     telefono: new FormControl('', Validators.required),
