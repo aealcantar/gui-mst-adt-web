9fad79f7d2e0504d8a32010f8302c747d769da92
lfgregorio <105452956+lfgregorio@users.noreply.github.com>
2022-06-27 19:23:39 -0500

    se agregan validaciones

--- src/app/nuevo-vdonacion-sangre/nuevo-vdonacion-sangre.component.ts
+++ src/app/nuevo-vdonacion-sangre/nuevo-vdonacion-sangre.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, NgForm, ValidatorFn } from '@angular/forms'
+import { Validators } from '@angular/forms'
 
 declare var $: any;
 
@@ -11,6 +12,34 @@ declare var $: any;
   styleUrls: ['./nuevo-vdonacion-sangre.component.css']
 })
 export class NuevoVdonacionSangreComponent implements OnInit {
+
+  formNuevoVolante: any = this.formBuilder.group({
+    idUnidadMedica: ['', Validators.required],
+    fechaUnidadMedica: ['', Validators.required],
+    nombreBancoSangre: ['', Validators.required],
+    horarioInicial: ['', Validators.required],
+    horarioFinal: ['', Validators.required],
+    codigoPostal: ['', Validators.required],
+    estado: ['', Validators.required],
+    deleMuni: ['', Validators.required],
+    ciudad: ['', Validators.required],
+    colonia: ['', Validators.required],
+    calle: ['', Validators.required],
+    numeroExterior: ['', Validators.required],
+    numeroInterior: ['', Validators.required],
+
+    nombrePaciente: ['', Validators.required],
+    servicio: ['', Validators.required],
+    fechaInternamiento: ['', Validators.required],
+    fechaCirugia: ['', Validators.required],
+    telefonoPaciente: ['', Validators.required],
+
+    nombreTS: ['', Validators.required],
+    matriculaTS: ['', Validators.required],
+    telefonoTS: ['', Validators.required],
+    observacionesTS: ['', Validators.required],
+
+  })
 
   constructor(
     private formBuilder: FormBuilder,
