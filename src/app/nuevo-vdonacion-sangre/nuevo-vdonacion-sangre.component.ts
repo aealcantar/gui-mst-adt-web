ba6538ce5faa65cc1f7251d547e1d6e91c3eaaa2
lfgregorio <105452956+lfgregorio@users.noreply.github.com>
2022-06-27 21:46:51 -0500

    se agrega validación

--- src/app/nuevo-vdonacion-sangre/nuevo-vdonacion-sangre.component.ts
+++ src/app/nuevo-vdonacion-sangre/nuevo-vdonacion-sangre.component.ts
@@ -3,19 +3,19 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, NgForm, ValidatorFn } from '@angular/forms'
 import { Validators } from '@angular/forms'
+import { Observable } from 'rxjs'
 
-declare var $: any;
+declare var $: any
 
 @Component({
   selector: 'app-nuevo-vdonacion-sangre',
   templateUrl: './nuevo-vdonacion-sangre.component.html',
-  styleUrls: ['./nuevo-vdonacion-sangre.component.css']
+  styleUrls: ['./nuevo-vdonacion-sangre.component.css'],
 })
 export class NuevoVdonacionSangreComponent implements OnInit {
-
   formNuevoVolante: any = this.formBuilder.group({
     idUnidadMedica: ['', Validators.required],
-    fechaUnidadMedica: ['', Validators.required],
+    // fechaUnidadMedica: ['', Validators.required],
     nombreBancoSangre: ['', Validators.required],
     horarioInicial: ['', Validators.required],
     horarioFinal: ['', Validators.required],
@@ -30,39 +30,39 @@ export class NuevoVdonacionSangreComponent implements OnInit {
 
     nombrePaciente: ['', Validators.required],
     servicio: ['', Validators.required],
-    fechaInternamiento: ['', Validators.required],
-    fechaCirugia: ['', Validators.required],
+    //    fechaInternamiento: ['', Validators.required],
+    //    fechaCirugia: ['', Validators.required],
     telefonoPaciente: ['', Validators.required],
 
     nombreTS: ['', Validators.required],
     matriculaTS: ['', Validators.required],
     telefonoTS: ['', Validators.required],
     observacionesTS: ['', Validators.required],
-
   })
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-  ) { }
+  filteredOptions!: Observable<any[]>
+  filterControl = new FormControl('')
 
-  ngOnInit(): void {
-  }
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {}
 
   modalcarga() {
     $('#content').modal({
       keyboard: false,
-      backdrop: 'static'
+      backdrop: 'static',
     })
-    $('#content').modal('show');
+    $('#content').modal('show')
   }
 
   cancelarModal() {
-    $('#content').modal('hide');
+    $('#content').modal('hide')
   }
 
   salirModal() {
-    this.router.navigateByUrl("/consulta-estudios-medicos", { skipLocationChange: true });
-    $('#content').modal('hide');
+    this.router.navigateByUrl('/consulta-estudios-medicos', {
+      skipLocationChange: true,
+    })
+    $('#content').modal('hide')
   }
 }
