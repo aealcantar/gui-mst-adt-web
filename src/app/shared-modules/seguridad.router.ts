5bedbb1c3b16e3d664005d96785b1b4dce1842d5
Miguel Angel Sanchez <miguel.sanchez@people-media.com.mx>
2022-06-29 01:08:10 -0500

    Se bajan cambios

---
+++ src/app/shared-modules/seguridad.router.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "./services/auth-service.service";
+
+
+@Injectable()
+export class SeguridadRouter implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (!this.authService.isAuthenticatedUser()) {
+      this.router.navigate(['/login'], { skipLocationChange: true });
+      return false;
+    }
+    return true;
+  }
+}
