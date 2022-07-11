d3400eb068d267899220172a8f1c68e47782f115
alanVillafan <108887328+alanVillafan@users.noreply.github.com>
2022-07-11 16:30:25 -0500

    correcion en la captura de horas
    se corrigio la captura de fechas en el certificado de defuncion

--- src/app/certificado-defuncion/nuevo-certificado/nuevo-certificado.component.ts
+++ src/app/certificado-defuncion/nuevo-certificado/nuevo-certificado.component.ts
@@ -1,43 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
 } from '@angular/forms';
+import * as moment from 'moment';
 import { CertificadoDefuncion } from 'src/app/models/certificado-defuncion.model';
 import { CertificadoDefuncionService } from 'src/app/service/certificado-defuncion.service';
 import { CronicaGrupalService } from 'src/app/service/cronica-grupal.service';
 
+declare var $: any;
 @Component({
   selector: 'app-nuevo-certificado',
   templateUrl: './nuevo-certificado.component.html',
   styleUrls: ['./nuevo-certificado.component.css'],
 })
-export class NuevoCertificadoComponent implements OnInit {
+export class NuevoCertificadoComponent implements OnInit ,AfterViewInit{
   formAdd;
-  certificado : CertificadoDefuncion;
+  certificado: CertificadoDefuncion;
   constructor(
     private formBuilder: FormBuilder,
     private cronicaGrupalService: CronicaGrupalService,
-    private certificadoService : CertificadoDefuncionService
+    private certificadoService: CertificadoDefuncionService
   ) {
     this.formAdd = formBuilder.group({
-      fechaDefuncion:new FormControl ('', Validators.required),
-      horaDefuncion:new FormControl ('', Validators.required),
-      foliofuncion:new FormControl ('', Validators.required),
-      nssPaciente:new FormControl ('', Validators.required),
-      cveServicio:new FormControl ('', Validators.required),
-      nombreAsegurado:new FormControl ('', Validators.required),
-      fechaDeEntregaDeCertificado:new FormControl ('', Validators.required),
-      horaDeEntregaDeCertificado:new FormControl ('', Validators.required),
-      nombreFuneraria:new FormControl ('', Validators.required),
-      nombreFamiliar:new FormControl ('', Validators.required),
-      parentescoFamiliar:new FormControl ('', Validators.required),
-      observaciones:new FormControl ('', Validators.required),
-      trabajadorSocial:new FormControl (''),
-      matricula:new FormControl (''),
+      fechaDefuncion: new FormControl('', Validators.required),
+      horaDefuncion: new FormControl('', Validators.required),
+      foliofuncion: new FormControl('', Validators.required),
+      nssPaciente: new FormControl('', Validators.required),
+      cveServicio: new FormControl('', Validators.required),
+      nombreAsegurado: new FormControl('', Validators.required),
+      fechaDeEntregaDeCertificado: new FormControl('', Validators.required),
+      horaDeEntregaDeCertificado: new FormControl('', Validators.required),
+      nombreFuneraria: new FormControl('', Validators.required),
+      nombreFamiliar: new FormControl('', Validators.required),
+      parentescoFamiliar: new FormControl('', Validators.required),
+      observaciones: new FormControl('', Validators.required),
+      trabajadorSocial: new FormControl(''),
+      matricula: new FormControl(''),
     });
+  }
+  ngAfterViewInit(): void {
+   $("#fhDefuncion").val().datepicker({
+    dateFormat: "dd/mm/yy",
+    onSelect: (date: any, datepicker: any) => {
+      if (date != '') {
+        date = moment(date, 'DD/MM/YYYY').format('YYYY-MM-DD');
+        this.formAdd.get('fechaDefuncion')?.patchValue(date);
+       // this.handleDatesChange();
+      }
+    },
+    onClose: (date: any) => {
+      if (!date) {
+        this.formAdd.get('fechaDefuncion')?.patchValue(null);
+      }
+    }
+  });
+  $("#entregaCertificado").val().datepicker({
+    dateFormat: "dd/mm/yy",
+    onSelect: (date: any, datepicker: any) => {
+      if (date != '') {
+        date = moment(date, 'DD/MM/YYYY').format('YYYY-MM-DD');
+        this.formAdd.get('fechaDeEntregaDeCertificado')?.patchValue(date);
+       // this.handleDatesChange();
+      }
+    },
+    onClose: (date: any) => {
+      if (!date) {
+        this.formAdd.get('fechaDeEntregaDeCertificado')?.patchValue(null);
+      }
+    }
+  });
   }
 
   listaServicios: Array<any> = [];
@@ -55,11 +89,10 @@ export class NuevoCertificadoComponent implements OnInit {
     if (this.formAdd.valid) {
       const certificado = this.formAdd.value as CertificadoDefuncion;
       console.log(certificado);
-      this.certificadoService.insert(certificado).subscribe(response=>{
-        console.log(response)
-        this.certificado = certificado
-      })
-      
+      this.certificadoService.insert(certificado).subscribe((response) => {
+        console.log(response);
+        this.certificado = certificado;
+      });
     } else {
       console.log(
         `'formulario no valido' ${JSON.stringify(this.formAdd.value)}`
