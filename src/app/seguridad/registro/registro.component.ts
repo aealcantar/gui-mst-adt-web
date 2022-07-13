020d7c92d04c33bb2a2902239cd214fc962f39a5
lfgregorio <105452956+lfgregorio@users.noreply.github.com>
2022-07-13 17:45:47 -0500

    alerts de recuperación de contraseña

--- src/app/seguridad/registro/registro.component.ts
+++ src/app/seguridad/registro/registro.component.ts
@@ -1,168 +1,194 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { SeguridadService } from '../seguridad.service';
-import { HttpClient, HttpErrorResponse, HttpEventType, HttpHeaders, HttpRequest, HttpResponse } from '@angular/common/http';
-import { AuthService } from 'src/app/service/auth-service.service';
-import { AdmonPasswordResponse } from 'src/app/models/admon-password-response.model';
-import { AdmonPasswordRequest } from 'src/app/models/admon-password-request.model';
-import { objAlert } from 'src/app/common/alerta/alerta.interface';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material/icon';
-
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router'
+import { SeguridadService } from '../seguridad.service'
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpEventType,
+  HttpHeaders,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http'
+import { AuthService } from 'src/app/service/auth-service.service'
+import { AdmonPasswordResponse } from 'src/app/models/admon-password-response.model'
+import { AdmonPasswordRequest } from 'src/app/models/admon-password-request.model'
+import { objAlert } from 'src/app/common/alerta/alerta.interface'
+import { DomSanitizer } from '@angular/platform-browser'
+import { MatIconRegistry } from '@angular/material/icon'
+import { AlertInfo } from 'src/app/app-alerts/app-alert.interface'
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
   logindata: any = this.formBuilder.group({
     nvacontrasenia: ['', Validators.required],
     confirmacontrasenia: ['', Validators.required],
-  });
-  submitted: boolean = false;
-  validaPassRegex: boolean = false;
-  correosubmitted: boolean = false;
-  staticAlertClosed: boolean = true;
-  mail:string='';
-  admonRequest:AdmonPasswordRequest=new AdmonPasswordRequest();
-  admonResponse:AdmonPasswordResponse=new AdmonPasswordResponse();
+  })
+  submitted: boolean = false
+  validaPassRegex: boolean = false
+  correosubmitted: boolean = false
+  staticAlertClosed: boolean = true
+  mail: string = ''
+  admonRequest: AdmonPasswordRequest = new AdmonPasswordRequest()
+  admonResponse: AdmonPasswordResponse = new AdmonPasswordResponse()
 
-  alert!: objAlert;
+  alert!: objAlert
 
-  showPassword1: boolean = false;
-  showPassword2: boolean = false;
+  showPassword1: boolean = false
+  showPassword2: boolean = false
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private seguridadService: SeguridadService,
     private route: ActivatedRoute,
-    private authService:AuthService,
+    private authService: AuthService,
     private domSanitizer: DomSanitizer,
     private matIconRegistry: MatIconRegistry,
-    private router: Router,private http: HttpClient) {
-
-      this.matIconRegistry.addSvgIcon(
-        "eye-on",
-        this.domSanitizer.bypassSecurityTrustResourceUrl(
-          "/assets/icons/eye-on-1.svg"
-        )
-      );
-      this.matIconRegistry.addSvgIcon(
-        "eye-off",
-        this.domSanitizer.bypassSecurityTrustResourceUrl(
-          "/assets/icons/eye-off.svg"
-        )
-      );
-    }
+    private router: Router,
+    private http: HttpClient,
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'eye-on',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '/assets/icons/eye-on-1.svg',
+      ),
+    )
+    this.matIconRegistry.addSvgIcon(
+      'eye-off',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '/assets/icons/eye-off.svg',
+      ),
+    )
+  }
 
   ngOnInit(): void {
-    this.route.queryParams
-    .subscribe(params => {
-      console.log(params); // { orderby: "price" }
-      this.mail = params['email'];
-      console.log(this.mail); // price
-    }
-  );
+    this.route.queryParams.subscribe((params) => {
+      console.log(params) // { orderby: "price" }
+      this.mail = params['email']
+      console.log(this.mail) // price
+    })
   }
 
   get f() {
-    return this.logindata.controls;
+    return this.logindata.controls
   }
 
   loginusuario(): void {
-    this.submitted = true;
-   console.log('valid'+this.logindata.valid);
+    this.submitted = true
+    console.log('valid' + this.logindata.valid)
 
-   if((this.logindata.value.nvacontrasenia != this.logindata.value.confirmacontrasenia)){
-    this.showError('<strong> Error </strong> ¡La contraseña no coincide, favor de verificar!')
-     return;
-   }
+    if (
+      this.logindata.value.nvacontrasenia !=
+      this.logindata.value.confirmacontrasenia
+    ) {
+      this.muestraAlerta(
+        '¡La contraseña no coincide, favor de verificar!',
+        'alert-danger',
+        'Error',
+      )
+      return
+    }
 
+    if (
+      this.logindata.valid &&
+      this.logindata.value.nvacontrasenia ==
+        this.logindata.value.confirmacontrasenia
+    ) {
+      const validate = (input: string) =>
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,10}$/.test(
+          input,
+        )
 
+      console.log(
+        `${this.logindata.value.nvacontrasenia} ${validate(
+          this.logindata.value.nvacontrasenia,
+        )}`,
+      )
+      if (validate(this.logindata.value.nvacontrasenia)) {
+        this.admonRequest.email = this.mail
+        this.admonRequest.password = this.logindata.value.nvacontrasenia
 
-    if (this.logindata.valid &&  (this.logindata.value.nvacontrasenia == this.logindata.value.confirmacontrasenia)) {
-
-      const validate = (input: string) => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,10}$/.test(input)
-
-      console.log(`${this.logindata.value.nvacontrasenia} ${validate(this.logindata.value.nvacontrasenia)}`)
-       if(validate(this.logindata.value.nvacontrasenia) ){
-        this.admonRequest.email=this.mail;
-        this.admonRequest.password=this.logindata.value.nvacontrasenia;
-
-
-              this.authService.actualizarPassword(this.admonRequest).subscribe(
-                (result) => {
-
-
-               console.log(result);
-               this.admonResponse=result;
-               if(this.admonResponse.status=="200")
-               {
-                this.showSucces('¡Éxito Contraseña guardada correctamente!', true);
-
-
-               }
-
-
-               },
-               (err: HttpErrorResponse) => {
-                 console.log("eror "+  err.error.message);
-                 this.showError('Ocurrio un error favor de reportarlo.')
-
-               }
-             );
-
-
-       }else {this.showError('<strong>Error </strong> ¡Contraseña inválida!');return;}
-
-
-
-
+        this.authService.actualizarPassword(this.admonRequest).subscribe(
+          (result) => {
+            console.log(result)
+            this.admonResponse = result
+            if (this.admonResponse.status == '200') {
+              this.muestraAlerta(
+                '¡Éxito Contraseña guardada correctamente!',
+                'alert-success',
+                null,
+              )
+              return
+            }
+          },
+          (err: HttpErrorResponse) => {
+            console.log('eror ' + err.error.message)
+            this.muestraAlerta(
+              'Ocurrio un error favor de reportarlo.',
+              'alert-danger',
+              'Error',
+            )
+          },
+        )
+      } else {
+        this.muestraAlerta(
+          '¡Éxito Contraseña guardada correctamente!',
+          'alert-success',
+          null,
+        )
+      }
     } else {
-      this.showError('¡Contraseña inválida!')
+      this.muestraAlerta('¡Contraseña inválida!', 'alert-danger', 'Error')
     }
   }
-  cancelarlogin(){
-    this.submitted = false;
-    this.logindata.reset();
-    this.router.navigate(["/login"]);
+  cancelarlogin() {
+    this.submitted = false
+    this.logindata.reset()
+    this.router.navigate(['/login'])
   }
 
-  private showError(error:string) {
-
+  private showError(error: string) {
     this.alert = {
-      message:error,
+      message: error,
       type: 'error',
-      visible: true
+      visible: true,
     }
     setTimeout(() => {
       this.alert = {
-        message:'',
+        message: '',
         type: 'custom',
-        visible: false
+        visible: false,
       }
-    }, 5000);
+    }, 5000)
   }
   //success
 
-  private showSucces(msg:string, redirect:boolean) {
-
+  muestraAlerta(
+    mensaje: string,
+    estilo: string,
+    tipoMsj?: string,
+    funxion?: any,
+  ) {
+    this.alert = new AlertInfo()
     this.alert = {
-      message:'<strong>Estatus.</strong>'+msg,
-      type: 'success',
-      visible: true
+      message: mensaje,
+      type: estilo,
+      visible: true,
+      typeMsg: tipoMsj,
     }
     setTimeout(() => {
       this.alert = {
-        message:'',
+        message: '',
         type: 'custom',
-        visible: false
+        visible: false,
       }
-      if(redirect){
-        this.router.navigate(["/login"], { skipLocationChange: true });
+      if (funxion != null) {
+        funxion()
       }
-    }, 5000);
+    }, 2000)
   }
-
 }
